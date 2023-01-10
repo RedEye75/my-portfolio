80205c9ebaedcbc7a15d94fe49df34a172c0a781
RedEye75 <yusufbinemamstk@gmail.com>
2023-01-11 00:25:05 +0600

    footer and route setup completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,24 +7,35 @@ const Contact = () => {
       <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-blue-500 uppercase rounded-full bg-teal-accent-400">
         Contact
       </p>
-      <h2 className="text-gray-500 mt-10 text-3xl font-bold">Lets talk</h2>
-      <p className="text-gray-500 text-xl  mt-3">
-        Fell free to contact with me via
-        <a
-          className="text-gray-500 hover:text-red-800 font-bold ml-2 text-xl"
-          href="mailto:yusufbinemamstk@gmail.com"
-        >
-          yusufbinemamstk@gmail.com
-        </a>
-      </p>
-      <p className="text-gray-500  text-xl ">
-        or fill the from at
-        <Link to={"/contactDetails"}>
-          <p className="text-gray-500 hover:text-red-800 inline font-bold ml-2 text-xl">
-            Contact Page
+      <div className="flex justify-around">
+        <div>
+          <h2 className="text-gray-500 mt-10 text-3xl font-bold">Lets talk</h2>
+          <p className="text-gray-500 text-xl  mt-3">
+            Fell free to contact with me via <br />
+            <a
+              className="text-gray-500 hover:text-red-800 font-bold ml-2 text-xl"
+              href="mailto:yusufbinemamstk@gmail.com"
+            >
+              yusufbinemamstk@gmail.com
+            </a>
           </p>
-        </Link>
-      </p>
+          <p className="text-gray-500  text-xl ">
+            or fill the from at
+            <Link to={"/contactDetails"}>
+              <p className="text-gray-500 hover:text-red-800 inline font-bold ml-2 text-xl">
+                Contact Page
+              </p>
+            </Link>
+          </p>
+        </div>
+        <div>
+          <img
+            className="w-72 mx-auto ml-10 mb-32"
+            src="https://cdn3d.iconscout.com/3d/premium/thumb/contact-us-5467852-4568171.png"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,44 +5,6 @@ const Footer = () => {
     <div className="mt-36 ">
       <footer className="text-gray-600 body-font ">
         {" "}
-        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a
-            href="https://web.facebook.com/profile.php?id=100032670966425"
-            className="text-gray-500"
-          >
-            <svg
-              fill="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-            </svg>
-          </a>
-
-          <a
-            href="https://www.linkedin.com/in/yusuf-bin-imam/"
-            className="ml-3 text-gray-500"
-          >
-            <svg
-              fill="currentColor"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="0"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="none"
-                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-              ></path>
-              <circle cx="4" cy="4" r="2" stroke="none"></circle>
-            </svg>
-          </a>
-        </span>
         <p
           id="text"
           className="text-xl text-gray-500 sm:ml-4 sm:pl-4  sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"
